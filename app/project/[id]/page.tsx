6c82771aa1b8d682950a8845b7933a7ef6a43488
Bryan Murgas <bryan.murgas@hotmail.com>
2026-02-08 11:09:05 -0500

    linked ideas floating dialog removed from the project view

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { requireAuth } from '@/lib/auth'
 import ProjectKanbanClient from '@/components/ProjectKanbanClient'
-import LinkedIdeasSection from '@/components/LinkedIdeasSection'
 
 export default async function ProjectPage({
   params,
@@ -9,17 +8,5 @@ export default async function ProjectPage({
 }) {
   await requireAuth()
 
-  return (
-    <>
-      <ProjectKanbanClient projectId={params.id} />
-      {/* 
-        Linked Ideas Section - Server component ready to integrate
-        Currently positioned as overlay. To integrate into RightPanel or main content,
-        pass projectId prop to ProjectKanbanClient and render LinkedIdeasSection inside.
-      */}
-      <div className="fixed bottom-4 right-4 w-96 max-h-96 overflow-y-auto z-50 shadow-lg">
-        <LinkedIdeasSection projectId={params.id} />
-      </div>
-    </>
-  )
+  return <ProjectKanbanClient projectId={params.id} />
 }
